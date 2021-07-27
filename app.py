703888db1190969d4a7779a9959444cc4cc62377
bzoliveira <beatrizmoreira_oliveira@outlook.com>
2021-07-27 19:22:31 -0300

    bea

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask,render_template
+from collections import namedtuple
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     iframe_do_carrosel = 'https://www.youtube.com/embed/AJ0bepmCeFo?&autoplay=1'
-    iframe_do_carrosel = 'https://www.youtube.com/embed/wD_Qu6qtx1g'
 
     return render_template(
         "index.html",iframe_do_carrosel=iframe_do_carrosel
     )
 @app.route("/servicos/")
 def servicos():
+
+    # Card = namedtuple('Card', ['src_imagem', 'alt_descricao'])
+
+    # codigo pra carregar json
+
+    card = {
+    "src_imagem":"link imagem",
+    "alt_descricao":"descricao alt",
+    "title":"tilte",
+    "text":"text"
+    }
+
+
     return render_template(
         "servicos.html"
     )
@@ -23,3 +36,5 @@ def cover():
 @app.route("/teste/<nome>/")
 def teste(nome):
     return f'<h1>{nome}</h1>'
+
+
