36fc3bb174462b42c3756603f803bfd86b8d3421
cesardddp <cesardddp@hotmail.com>
2021-07-27 17:58:58 -0300

    adcionado pag de serviços add galeria add bg-branco-azul add jblock style e link style alterado footer alterado esquema de carregamento de conteudo (url for do jinja)

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask,render_template,url_for
-from historia import historia
+from flask import Flask, render_template, url_for
+from conteudo import (
+    historia,
+    aulas
+)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    iframe_do_carrosel = 'https://www.youtube.com/embed/AJ0bepmCeFo'
-    banner2 = url_for('static',filename='imgs/banner2.jpg')
-    banner1 = url_for('static',filename='imgs/banner1.jpg')
+    iframe_do_carrosel = "https://www.youtube.com/embed/AJ0bepmCeFo"
+    banner2 = url_for("static", filename="imgs/banner2.jpg")
+    banner1 = url_for("static", filename="imgs/banner1.jpg")
     # import pdb;pdb.set_trace()
     return render_template(
         "index.html",
         iframe_do_carrosel=iframe_do_carrosel,
         banner2=banner2,
         banner1=banner1,
-        historia=historia
+        historia=historia,
     )
+
+
 @app.route("/servicos/")
 def servicos():
-    return render_template(
-        "servicos.html"
-    )
-@app.route("/cover/")
-def cover():
-    return render_template(
-        "cover.html"
-    )
+    return render_template("servicos2.html",aulas=aulas)
+
+@app.route("/galeria/")
+def galeria():
+
+    import os
+    import itertools
+    lista_fotos = os.listdir('static/imgs/galeria')
+    lista_fotos = list(itertools.zip_longest(*[iter(lista_fotos)]*4,fillvalue=""))
+
+    return render_template("galeria.html",fotos=lista_fotos)
+
+@app.route("/contato/")
+def contato():
+    return render_template("contat.html")
---
+++ conteudo.py
@@ -0,0 +1,84 @@
+# INDEX
+
+historia = {
+        "parte1": {
+            "titulo": "Manxs",
+            "subtitulo": "(Grupo de Arte Drag Queen)",
+            "conteudo": "Atua com performances musicais (dança e canto) sobre a influência do estilo Rock, como ferramenta para o empoderamento e entretenimento da comunidade LGBT.",
+            "img":{
+                "src":"static/imgs/historia1.jpg",#imagem de 500x500
+                "alt":""}
+            },
+        "parte2": {
+                "titulo": "Missão:",
+                "subtitulo": "Fazer do fervo, um ato politico",
+                "conteudo": "",
+                "img":{
+                    "src":"static/imgs/historia2.jpg",#imagem de 500x500
+                    "alt":""}
+            },
+        "parte3": {
+                "titulo": "Objetivo do grupo:",
+                "subtitulo": "",
+                "conteudo": "Fazer da música e da dança, um o ponto de partida e ferramenta para visibilidade e empoderamento da comunidade LGBT.",
+                "img":{
+                    "src":"static/imgs/historia3.jpg",#imagem de 500x500
+                    "alt":""}
+            }
+}
+
+# SERVIÇOS
+
+cursos = [
+        {
+            "tema": "canto",
+            "titulo": "titulo curso",
+            "subtitulo": "subtitulo",
+            "conteudo": "oque é o curso curso curso",
+            "link": "https://www.instagram.com/keima.kengaral/",
+            "imagem": {
+                # "src": url_for("static", filename="imgs/aula_exmp_pedre.jpg"),
+                "src": "aula_exmp_pedre.jpg",
+                "alt": "alt descrição",
+            },
+        },
+
+]
+
+aulas = [
+        {
+            "tema": "canto",
+            "titulo": "pedre",
+            "subtitulo": "subtitulo",
+            "conteudo": "conteudo conteudo conteudo conteudo conteudo conteudo ",
+            "link": "https://www.instagram.com/keima.kengaral/",
+            "imagem": {
+                # "src": url_for("static", filename="imgs/aula_exmp_pedre.jpg"),
+                "src": "aula_exmp_pedre.jpg",
+                "alt": "alt descrição",
+            },
+        },
+        {
+            "tema": "dança",
+            "titulo": "luisaff",
+            "subtitulo": "subtitulo",
+            "conteudo": "conteudo conteudo conteudo conteudo conteudo conteudo ",
+            "link": "https://www.instagram.com/oluisaff/",
+            "imagem": {
+                "src": "aula_exmp_luis.jpg",
+                # "src": url_for("static", filename="imgs/aula_exmp_luis.jpg"),
+                "alt": "alt descrição",
+            },
+        },
+        {
+            "tema": "maquiagem",
+            "titulo": "viccky",
+            "subtitulo": "subtitulo",
+            "conteudo": "conteudo conteudo conteudo conteudo conteudo conteudo ",
+            "link": "https://www.instagram.com/poxaleo_eu/",
+            "imagem": {
+                "src": "aula_exmp_leo.jpg",
+                "alt": "alt descrição",
+            },
+        },
+    ]
