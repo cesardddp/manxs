2839c2aa5c1520d0bb8d8f76a0fea3fee41d1b7d
cesardddp <cesardddp@hotmail.com>
2022-02-10 21:31:09 -0300

    componentizer biografia

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for
 import itertools
-from conteudo import historia, aulas, oficinas, videos_lista, cursos
-from conteudo2 import (
+from conteudo import ( historia, aulas, oficinas, videos_lista, cursos,
     redes_sociais_componente,
-    biografia as biografia_conteudo,
-    instagrams,
+)
+from data.biografia import (
+    TITULO,
+    BANNER,
+    TEXTO_PRINCIPAL,
+    FEITOS,
+    TEXTO_HISTORICO,
+    MUSICAS,
+    TEXTO_FINAL,
+    CARD_MISSAO,
+    CARD_OBJETIVO,
+    INSTAGRAMS
 )
 import json
 from flask_minify import minify, decorators
@@ -39,21 +48,40 @@ def index():
         m_banner3=m_banner3,
         historia=historia,
         redes_sociais_componente=redes_sociais_componente,
+        render_id=False,
     )
 
 
 @app.route("/biografia/")
 @decorators.minify(html=True, js=True, cssless=True)
-# @app.route("/biografia/")
 def biografia():
 
-    # import pdb;pdb.set_trace()
+    titulo_data: dict = TITULO
+    banner_data: dict = BANNER
+    texto_principal_data: dict = TEXTO_PRINCIPAL
+    feitos_data: dict = FEITOS
+    texto_historico_data: dict = TEXTO_HISTORICO
+    musicas_data: dict = MUSICAS
+    texto_final_data: dict = TEXTO_FINAL
+    card_missao_data: dict = CARD_MISSAO
+    card_objetivo_data: dict = CARD_OBJETIVO
+    instagrams_data:dict =INSTAGRAMS
+
+
     return render_template(
         "biografia.html",
-        historia=historia,
         redes_sociais_componente=redes_sociais_componente,
-        biografia=biografia_conteudo,
-        instagrams=instagrams,
+        titulo_data=titulo_data,
+        banner_data=banner_data,
+        texto_principal_data=texto_principal_data,
+        feitos_data=feitos_data,
+        texto_historico_data=texto_historico_data,
+        musicas_data=musicas_data,
+        texto_final_data=texto_final_data,
+        card_missao_data=card_missao_data,
+        card_objetivo_data=card_objetivo_data,
+        instagrams_data=instagrams_data,
+        render_id=False,
     )
 
 
@@ -62,7 +90,9 @@ def biografia():
 def servicos():
     cards = list(itertools.zip_longest(*[iter(oficinas)] * 3, fillvalue=""))
     # cards é uma lista de listas [ [card1,card2,card3],[card4,card5,card6],... ]
-    return render_template("servicos.html", aulas=aulas, cards=cards, cursos=cursos)
+    return render_template(
+        "servicos.html", aulas=aulas, cards=cards, cursos=cursos, render_id=False
+    )
 
 
 @app.route("/galeria/")
@@ -75,7 +105,7 @@ def galeria():
 
     lista_fotos = list(itertools.zip_longest(*[iter(lista_fotos)] * 3, fillvalue=""))
     # import ipdb;ipdb.set_trace()
-    return render_template("galeria.html", fotos=set(lista_fotos))
+    return render_template("galeria.html", fotos=set(lista_fotos), render_id=False)
 
 
 @app.route("/formulario/")
@@ -110,31 +140,35 @@ def form_curso_example():
         "SE": "Sergipe",
         "TO": "Tocantins",
     }
-    return render_template("forms/curso.html", estados=estados)
+    return render_template("forms/curso.html", estados=estados, render_id=False)
 
 
 @app.route("/videos/")
 @decorators.minify(html=True, js=True, cssless=True)
 def videos():
     json.load(open("videos.json"))
-    return render_template("videos.html", videos=json.load(open("videos.json")))
+    return render_template(
+        "videos.html", videos=json.load(open("videos.json")), render_id=False
+    )
 
 
 @app.route("/contato/")
 @decorators.minify(html=True, js=True, cssless=True)
 def contato():
     return render_template(
-        "contato.html", redes_sociais_componente=redes_sociais_componente
+        "contato.html",
+        redes_sociais_componente=redes_sociais_componente,
+        render_id=False,
     )
 
 
 @app.route("/shows/")
 @decorators.minify(html=True, js=True, cssless=True)
 def shows():
-    return render_template("shows.html")
+    return render_template("shows.html", render_id=False)
 
 
 @app.route("/musicas/")
 @decorators.minify(html=True, js=True, cssless=True)
 def musicas():
-    return render_template("musicas.html")
+    return render_template("musicas.html", render_id=False)
