d5f46b336ab41c5b718298d01975b48025107c9b
cesardddp <cesardddp@hotmail.com>
2022-02-24 12:17:53 -0300

    autoupload

---
+++ static/editor.js
@@ -0,0 +1,50 @@
+const Editor = {
+        compilerOptions: {
+            delimiters: ['@{', '}']
+        },
+        data() {
+            return {
+                titulo_data:"",
+                banner_data:"",
+                feitos_data:"",
+                texto_historico_data:"",
+                musicas_data:"",
+                texto_final_data:"",
+                texto_principal_data:"",
+                card_missao_data:"",
+                card_objetivo_data:"",
+                instagrams_data:"",
+}
+        },
+        methods: {
+            teste: _.debounce(function (event) {
+                console.log(
+                    // this.$data
+                )
+                let text = event.target.textContent
+                let id = event.target.id
+                debugger
+
+                axios
+                    .patch(
+                        '/editor/edit/'+id,
+                        {data:text}
+                        //substitui pela url de busca
+                    )
+                    .then(response => {
+                        debugger
+                        
+                    })
+                    .catch(error => {
+                        alert('Erro! Não foi possível adquirir resultados da API: ' + error)
+                    })
+
+            }, 550)
+
+            }
+        
+        
+    }
+    const app = Vue.createApp(Editor)
+    app.mount("#app_editor")
+    
